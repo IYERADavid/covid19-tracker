391f6c97022701af1b77a05d1afc1968487bb100
David Iyera <iyeradavid@gmail.com>
2025-07-18 19:07:37 +0200

    add footer and refine header

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,15 @@
 function Footer() {
     return (
         <div>
-            
+            <footer class="bg-light text-dark pt-1 pb-3">
+                <div class="container text-md-left">
+                    <div class="row mt-4">
+                    <div class="col-md-12 text-center">
+                        <p class="mb-0">&copy; 2025 Covid 19 Tracker. All rights reserved.</p>
+                    </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -4,21 +4,13 @@ function Header() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link to="/" className="navbar-brand" >Covid 19</Link>
+          <Link to="/" className="navbar-brand fw-bold fs-4" >Covid 19 Tracker</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item mx-5">
-                <Link to="/" className="nav-link" aria-current="page" >Home</Link>
-              </li>
-              <li className="nav-item mx-5">
-                <Link to="/news" className="nav-link" >Recent News</Link>
-              </li>
-              <li className="nav-item mx-5">
-                <Link to="/contact" className="nav-link" >Contact Us</Link>
-              </li>
+               {/* TODO: add necessary navigation links  */}
             </ul>
           </div>
         </div>
